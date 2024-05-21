ad102df8473b95c773ab0cbd24c8734fe59043f4
meltempolat <polat.meltm@gmail.com>
2024-05-21 13:41:30 +0300

    String örnek uygulama

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log("merhaba");
+let url = "https://www.meltempolat.com/";
+let kursAdi = "JavaScript çalışma"
+
+//url kaç karakterlidir
+
+let sonuc;
+
+sonuc = url.length;
+
+
+//Kurs adı kaç kelimeden oluşmaktadır
+
+sonuc = kursAdi.split(" ").length;
+
+// url https ile mi başlıyor 
+
+sonuc = url.startsWith("https");
+
+if(sonuc) {
+    console.log("evet");
+}
+
+//url ve kursAdi değişkenlerini kullanarak şu stringi oluştur
+//https://www.meltempolat.com/JavaScript-çalışma"
+
+kursAdi = kursAdi.toLowerCase();
+kursAdi = kursAdi.replaceAll(" ", "-")
+
+sonuc = '${url}/${kursAdi}';
+
+console.log(sonuc);
+
+
+
+
+
+
+
+
+
+console.log(sonuc);
