d3d8d925626dc9fb13470b0efc2aead1f971933b
meltempolat <polat.meltm@gmail.com>
2024-05-24 13:47:31 +0300

    Constructor

--- script.js
+++ script.js
@@ -15,22 +15,46 @@ return cevap === this.dogruCevap
     }
 }
 
-let soru1 ={
-    soruMetni: "Hangisi .net uygulamasidir?",
+// let soru1 ={
+//     soruMetni: "Hangisi .net uygulamasidir?",
 
-    cevapSeçenekleri: {
-        a: "Node.js",
-        b: "nuget",
-        c: "Npm"
-    },
+//     cevapSeçenekleri: {
+//         a: "Node.js",
+//         b: "nuget",
+//         c: "Npm"
+//     },
 
-    dogruCevap:"b",
-    cevabiKontrolEt: function(cevap) {
-return cevap === this.dogruCevap
-    }
+//     dogruCevap:"b",
+//     cevabiKontrolEt: function(cevap) {
+// return cevap === this.dogruCevap
+//     }
+// }
+
+
+function Soru(soruMetni, cevapSeçenekleri,dogruCevap) {
+    this.soruMetni = soruMetni;
+    this.cevapSeçenekleri = cevapSeçenekleri;
+    this.dogruCevap = dogruCevap;
+
 }
 
+let soru1 = new Soru("Hangisi JavaScript uygulamasidir?", {a: "Node.js",b: "TypeScript",c: "Npm"}, "c");
+let soru2 = new Soru("Hangisi .net uygulamasidir?", {a: "Node.js",b: "nuget",c: "Npm"}, "b");
 
-console.log(soru.soruMetni);
-console.log(soru.cevabiKontrolEt("c"));
-console.log(soru.cevabiKontrolEt("c"));
+
+
+let sorular =[
+
+new Soru("Hangisi JavaScript uygulamasidir?", {a: "Node.js",b: "TypeScript",c: "Npm"}, "c"),
+new Soru("Hangisi JavaScript uygulamasidir?", {a: "Node.js",b: "TypeScript",c: "Npm"}, "c"),
+new Soru("Hangisi JavaScript uygulamasidir?", {a: "Node.js",b: "TypeScript",c: "Npm"}, "c"),
+new Soru("Hangisi JavaScript uygulamasidir?", {a: "Node.js",b: "TypeScript",c: "Npm"}, "c"),
+
+]
+console.log(sorular[0].soruMetni);
+
+
+console.log(soru1.soruMetni);
+console.log(soru1.dogruCevap);
+
+
